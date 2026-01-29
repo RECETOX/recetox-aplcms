c03993c5b4e33e25810d2b1fa82a836b2e11d403
Ondrej Bucek <ondrej.bucek@protonmail.com>
2026-01-29 13:17:13 +0100

    Tests for compute_clusters_sd function - parametrised test will fail because the data were not updated yet. If the function compute_clusters_sd works as intended, tests wil be updated.

--- tests/testthat/test-compute_clusters.R
+++ tests/testthat/test-compute_clusters.R
@@ -70,3 +70,118 @@ test_that("compute clusters simple", {
 
   expect_equal(as.list(actual), expected, tolerance = 0.02)
 })
+
+# parametrised test for compute_clusters_sd function
+patrick::with_parameters_test_that(
+  "test compute_clusters_sd",
+  {
+    testdata <- file.path("..", "testdata")
+
+    extracted <- read_parquet_files(files, input, ".parquet")
+
+    actual <- compute_clusters_sd(
+      feature_tables = extracted,
+      mz_tol_relative = NA,
+      rt_tol_relative = NA,
+      mz_max_diff = mz_max_diff,
+      mz_tol_absolute = mz_tol_absolute,
+      sd_ratio_tol_relative = NA,
+      do.plot = FALSE,
+      sample_names = files
+    )
+
+    # for(i in 1:3) {
+    #   arrow::write_parquet(actual$feature_tables[[i]], file.path(testdata, "clusters", paste0(files[i], "_", input ,"_clusters.parquet")))
+    # }
+    expected <- read_parquet_files(files, "clusters", paste0("_", input, "_clusters.parquet"))
+
+
+    for(i in seq_along(files)) {
+      expect_equal(actual$feature_tables[[i]], expected[[i]])
+    }
+
+    expect_equal(actual$mz_tol_relative, expected_mz_tol_relative, tolerance=0.1)
+    expect_equal(actual$rt_tol_relative, expected_rt_tol_relative, tolerance=0.1)
+
+  },
+  patrick::cases(
+    RCX_shortened_extracted = list(
+      files = c("RCX_06_shortened", "RCX_07_shortened", "RCX_08_shortened"),
+      input = "extracted",
+      mz_max_diff = 10 * 1e-05,
+      mz_tol_absolute = 0.01,
+      expected_mz_tol_relative = 6.849039e-06,
+      expected_rt_tol_relative = 2.894385
+    ),
+    RCX_shortened_adjusted = list(
+      files = c("RCX_06_shortened", "RCX_07_shortened", "RCX_08_shortened"),
+      input = "adjusted",
+      mz_max_diff = 10 * 1e-05,
+      mz_tol_absolute = 0.01,
+      expected_mz_tol_relative = 6.856763e-06,
+      expected_rt_tol_relative = 1.93185408267324
+    )
+  )
+)
+
+
+# unit test for new compute_clusters_sd function
+test_that("compute_clusters_sd()", {
+  sample_names <- c("sample_1", "sample_2")
+
+  # input data for unit test - sample 1
+  features_1 <- dplyr::tibble(
+    mz = c(1,2,6,6,7,7),
+    rt = c(1,1,2,3,4,4),
+    sd1 = c(1,1,1,1,3,3),
+    sd2 = c(1,1,1,1,3,1),
+    area = c(10,10,10,10,10,10)
+  )
+
+  # expected output
+  expected_1 <- dplyr::tibble(
+    mz = c(1,2,6,6,7,7),
+    rt = c(1,1,2,3,4,4),
+    sd1 = c(1,1,1,1,3,3),
+    sd2 = c(1,1,1,1,3,1),
+    area = c(10,10,10,10,10,10),
+    sample_id = rep("sample_1", 6),
+    sd_ratio = c(1,1,1,1,1,3),
+    cluster = c(2,3,4,5,6,7)
+  )
+
+  # input data for unit test - sample 2
+  features_2 <- dplyr::tibble(
+    mz = c(1,2,6,6,7,7),
+    rt = c(1,1,2,3,4,4),
+    sd1 = c(1,1,1,1,3,3),
+    sd2 = c(1,1,1,1,3,1),
+    area = c(10,10,10,10,10,10)
+  )
+
+  # expected output
+  expected_2 <- dplyr::tibble(
+    mz = c(1,2,6,6,7,7),
+    rt = c(1,1,2,3,4,4),
+    sd1 = c(1,1,1,1,3,3),
+    sd2 = c(1,1,1,1,3,1),
+    area = c(10,10,10,10,10,10),
+    sample_id = rep("sample_2", 6),
+    sd_ratio = c(1,1,1,1,1,3),
+    cluster = c(2,3,4,5,6,7)
+  )
+
+  actual <- compute_clusters_sd(feature_tables = list(features_1, features_2),
+                             mz_tol_relative = 0.9,
+                             mz_tol_absolute = 0.9,
+                             mz_max_diff = 1e-04,
+                             rt_tol_relative = 0.9,
+                             sd_ratio_tol_relative = 0.9,
+                             do.plot = FALSE,
+                             sample_names = sample_names
+  )
+
+  expect_equal(length(actual$feature_tables), 2)
+  expect_equal(actual$feature_tables[[1]], expected_1)
+  expect_equal(actual$feature_tables[[2]], expected_2)
+})
