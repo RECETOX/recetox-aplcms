3057fc24646a63ec718bcd5eb27a9e110209a4af
Michaela Bodova <505814@muni.cz>
2026-06-30 08:40:44 +0000

    added test for sample_id column

--- tests/testthat/test-load.lcms.R
+++ tests/testthat/test-load.lcms.R
@@ -26,7 +26,7 @@ patrick::with_parameters_test_that(
     }
 
     # Act: Execute the function with the test inputs
-    data <- load.lcms(filename)
+    data <- load.lcms(filename) |> dplyr::select(-sample_id)
 
     # Assert: Verify the function output matches expected results
     # Check that the function returns an object of the expected type
@@ -48,3 +48,39 @@ patrick::with_parameters_test_that(
     # test_case_3 = create_test_case(rawrr::sampleFilePath(), 30689, 30689, 30689)
   )
 )
+
+patrick::with_parameters_test_that(
+  "sample_id is loaded and saved correctly",{
+
+    # Skip if rawrr is not installed for raw files
+    if (tools::file_ext(filename) == "raw" && !requireNamespace("rawrr", quietly = TRUE)) {
+      testthat::skip("The 'rawrr' package is required for reading raw files but is not installed.")
+    }
+
+    # Check if path is absolute or relative, if relative, prepend testdata directory
+    if (!file.exists(filename)) {
+      filename <- file.path("..", "testdata", "input", filename)
+    }
+
+    if(ci_skip == TRUE) skip_on_ci()
+    
+    actual <- load.lcms(filename)
+
+    testthat::expect_equal(actual$sample_id, sample_id)
+
+    patrick::cases(
+      mzml_file1 = list(filename="tests/testdata/input/RCX_06_shortened.mzML", 
+                      sample_id = "data_06", 
+                      ci_skip = FALSE),
+                      
+      mzml_file2 = list(filename="tests/testdata/input/RCX_07_shortened.mzML", 
+                      sample_id = "data_07", 
+                      ci_skip = FALSE),
+
+      raw_file = list(filename="tests/testdata/input/8_qc_no_dil_milliq.raw",   
+                      sample_id = "8_qc_no_dil_milliq", 
+                      ci_skip = TRUE)
+      
+    )
+
+})
